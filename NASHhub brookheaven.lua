abecebdc669b9ccd6740e1afd8d4c69f037f5065
nandowutwut <hernando7a@gmail.com>
2025-05-12 17:21:39 +0700

    Create NASHhub brookheaven.lua

---
+++ NASHhub brookheaven.lua
@@ -0,0 +1,29 @@
+if game.PlaceId == 4924922222 then
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Brookhaven scrip nashub",
+   LoadingTitle = "welcome to NASHhub",
+   LoadingSubtitle = "NASHhub",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = WUT,
+      FileName = "ByNASHhub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "https://discord.gg/6JH3wJJJ",
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "NASHHub|KeySystem",
+      Subtitle = "KeySystem NASHhub",
+      Note = "JANGAN LUPA JOIN DISCORD",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = true,
+      Key = {"RRQ EVOS SAHABATAN"}
+  }
+    }
